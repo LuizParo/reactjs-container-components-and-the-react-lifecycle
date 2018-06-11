1945bf58da28a30a004560c0e82b89e337241149
Luiz Guilherme Paro <lparo@daitangroup.com>
2018-06-11 11:43:48 -0300

    communicating with the api using the fetch API and making the FotoItem component dynamic

--- src/components/FotoItem.js
+++ src/components/FotoItem.js
@@ -3,18 +3,20 @@ import React, { Component } from 'react';
 class FotoHeader extends Component {
 
     render() {
+        const { urlPerfil, loginUsuario, horario } = this.props.foto;
+
         return (
             <header className="foto-header">
                 <figure className="foto-usuario">
-                    <img src="https://instagram.fcgh10-1.fna.fbcdn.net/t51.2885-19/11199408_569104449895751_1837574990_a.jpg" alt="foto do usuario"/>
+                    <img src={urlPerfil} alt="foto do usuario"/>
                     <figcaption className="foto-usuario">
                         <a href="#">
-                            alots
+                            {loginUsuario}
                         </a>  
                     </figcaption>
                 </figure>
 
-                <time className="foto-data">03/10/2016 20:13</time>
+                <time className="foto-data">{horario}</time>
             </header>
         );
     }
@@ -23,40 +25,34 @@ class FotoHeader extends Component {
 class FotoInfo extends Component {
 
     render() {
+        const { likers, comentario, comentarios } = this.props.foto;
+
         return (
             <div className="foto-in fo">
                 <div className="foto-info-likes">
-                    <a href="#">
-                        alots_ssa
-                    </a>
-
-                    ,
-
-                    <a href="#">
-                        rafael_rollo
-                    </a> 
+                    {
+                        likers.map(liker => <a href="#">{liker.login}, </a>)
+                    }
 
                     curtiram
                 </div>
 
                 <p className="foto-info-legenda">
                     <a className="foto-info-autor">autor </a>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, illo?
+                    {comentario}
                 </p>
 
                 <ul className="foto-info-comentarios">
-                    <li className="comentario">
-                        <a className="foto-info-autor">seguidor </a>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem ad, molestiae.
-                    </li>
-                    <li className="comentario">
-                        <a className="foto-info-autor">seguidor </a>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt cumque earum molestias voluptatem modi nihil sit magnam ratione eveniet distinctio magni error asperiores dignissimos tempora expedita, laborum ex soluta hic maiores veritatis deserunt.
-                    </li>
-                    <li className="comentario">
-                        <a className="foto-info-autor">seguidor </a>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum laudantium quae ab fuga odio delectus maiores voluptatibus sit commodi quidem.
-                    </li>
+                    {
+                        comentarios.map(comentario => {
+                            return (
+                                <li className="comentario">
+                                    <a className="foto-info-autor">{comentario.login} </a>
+                                    {comentario.texto}
+                                </li>
+                            );
+                        })
+                    }
                 </ul>
             </div>
         );
@@ -78,15 +74,17 @@ class FotoAtualizacoes extends Component {
     }
 }
 
-export default class Foto extends Component {
-
+export default class FotoItem extends Component {
+    
     render() {
+        const { foto } = this.props;
+
         return (
             <div className="foto">
-                <FotoHeader/>
-                <img alt="foto" className="foto-src" src="https://instagram.fcgh10-1.fna.fbcdn.net/t51.2885-15/e35/14482111_1635089460122802_8984023070045896704_n.jpg?ig_cache_key=MTM1MzEzNjM4NzAxMjIwODUyMw%3D%3D.2"/>
-                <FotoInfo/>
-                <FotoAtualizacoes/>
+                <FotoHeader foto={foto} />
+                <img alt="foto" className="foto-src" src={foto.urlFoto} />
+                <FotoInfo foto={foto} />
+                <FotoAtualizacoes />
             </div>
         );
     }
--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 
-import Foto from './Foto';
+import FotoItem from './FotoItem';
 
 export default class Timeline extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            fotos : []
+        };
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:8080/api/public/fotos/rafael')
+            .then(response => response.json())
+            .then(fotos => this.setState({ fotos }));
+    }
 
     render() {
         return (
             <div className="fotos container">
-                <Foto/>
-                <Foto/>
+                {
+                    this.state.fotos.map(foto => <FotoItem foto={foto} />)
+                }
             </div>
         );
     }
